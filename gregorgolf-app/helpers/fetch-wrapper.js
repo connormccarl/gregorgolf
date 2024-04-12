bdf2bdd977f4748a13f10a2fa8d9adbad26e9c5d
Connor McCarl <connormccarl@gmail.com>
2024-04-12 11:05:47 -0700

    removed autologout on 401 error

--- gregorgolf-app/helpers/fetch-wrapper.js
+++ gregorgolf-app/helpers/fetch-wrapper.js
@@ -52,7 +52,7 @@ async function handleResponse(response) {
     if (!response.ok) {
         if ([401, 403].includes(response.status) && userService.userValue) {
             // auto logout if 401 Unauthorized or 403 Forbidden response returned from api
-            userService.logout();
+            //userService.logout();
         }
 
         // get error message from body or default to response status
